5d7cfdb81c63be1564027c94d682e02b11368f4d
Caden <cnewt2003>
2023-08-06 18:55:04 -0700

    feat(homepage): add logo + refactor profile button

---
+++ src/components/Navbar/ProfileButton.tsx
@@ -0,0 +1,65 @@
+import {
+  alpha,
+  Avatar,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import React from 'react';
+
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const ProfileButton = () => {
+  const theme = useTheme();
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null,
+  );
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 0 }}>
+      <Tooltip title="Open settings">
+        <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+          <Avatar sx={{ bgcolor: alpha(theme.palette.secondary.main, 0.15) }}>
+            C
+          </Avatar>
+        </IconButton>
+      </Tooltip>
+      <Menu
+        sx={{ mt: '45px' }}
+        id="menu-appbar"
+        anchorEl={anchorElUser}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={Boolean(anchorElUser)}
+        onClose={handleCloseUserMenu}
+      >
+        {settings.map((setting) => (
+          <MenuItem key={setting} onClick={handleCloseUserMenu}>
+            <Typography textAlign="center">{setting}</Typography>
+          </MenuItem>
+        ))}
+      </Menu>
+    </Box>
+  );
+};
+
+export default ProfileButton;
--- src/components/Navbar/TopBar.tsx
+++ src/components/Navbar/TopBar.tsx
@@ -1,40 +1,18 @@
 import MenuIcon from '@mui/icons-material/Menu';
-import {
-  alpha,
-  AppBar,
-  Avatar,
-  Box,
-  IconButton,
-  Menu,
-  MenuItem,
-  Toolbar,
-  Tooltip,
-  Typography,
-} from '@mui/material';
+import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import * as React from 'react';
 
+import Logo from './Logo';
+import ProfileButton from './ProfileButton';
 import SearchBar from './SearchBar';
 
 interface TopBarProps {
   toggleDrawer: () => void;
 }
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
 const TopBar = ({ toggleDrawer }: TopBarProps) => {
   const theme = useTheme();
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null,
-  );
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
 
   return (
     <AppBar position="fixed" sx={{ zIndex: theme.zIndex.drawer + 1 }}>
@@ -49,50 +27,11 @@ const TopBar = ({ toggleDrawer }: TopBarProps) => {
         >
           <MenuIcon />
         </IconButton>
-        <Typography
-          variant="h6"
-          noWrap
-          component="div"
-          sx={{ display: { xs: 'none', sm: 'block' } }}
-        >
-          MUI
-        </Typography>
+        <Logo />
         <Box sx={{ margin: 'auto' }}>
           <SearchBar />
         </Box>
-        <Box sx={{ flexGrow: 0 }}>
-          <Tooltip title="Open settings">
-            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-              <Avatar
-                sx={{ bgcolor: alpha(theme.palette.secondary.main, 0.15) }}
-              >
-                C
-              </Avatar>
-            </IconButton>
-          </Tooltip>
-          <Menu
-            sx={{ mt: '45px' }}
-            id="menu-appbar"
-            anchorEl={anchorElUser}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            open={Boolean(anchorElUser)}
-            onClose={handleCloseUserMenu}
-          >
-            {settings.map((setting) => (
-              <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                <Typography textAlign="center">{setting}</Typography>
-              </MenuItem>
-            ))}
-          </Menu>
-        </Box>
+        <ProfileButton />
       </Toolbar>
     </AppBar>
   );
