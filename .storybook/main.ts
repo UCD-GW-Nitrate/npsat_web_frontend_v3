0c29244f0808462b055e8553d8df50494e912664
Caden Newton <cnewton@ucdavis.edu>
2023-09-03 00:33:11 -0700

    feat(storybook): aet up storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-styling',
   ],
   framework: {
     name: '@storybook/nextjs',
@@ -16,6 +17,22 @@ const config: StorybookConfig = {
   },
   core: {
     disableTelemetry: true,
+  },
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      // makes union prop types like variant and size appear as select controls
+      shouldExtractLiteralValuesFromEnum: true,
+      // makes string and boolean types that can be undefined appear as inputs and switches
+      shouldRemoveUndefinedFromOptional: true,
+      // Filter out third-party props from node_modules except @mui packages
+      propFilter: (prop) =>
+        prop.parent
+          ? !/node_modules\/(?!@mui)/.test(prop.parent.fileName)
+          : true,
+    },
   },
 };
 
