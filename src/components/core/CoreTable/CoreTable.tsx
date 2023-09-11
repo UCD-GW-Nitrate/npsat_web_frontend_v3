c2edfa09420641129719db3004537ebadb657b47
Caden <cnewt2003>
2023-09-11 03:18:02 -0700

    feat(homepage): improvements to table

--- src/components/core/CoreTable/CoreTable.tsx
+++ src/components/core/CoreTable/CoreTable.tsx
@@ -4,12 +4,12 @@ import {
   Table,
   TableBody,
   TableCell,
-  TableContainer,
   TableHead,
   TablePagination,
   TableRow,
 } from '@mui/material';
-import React from 'react';
+import type { UIEvent } from 'react';
+import React, { useState } from 'react';
 
 import type { PlotModel } from '@/store/apis/feedApi';
 
@@ -40,50 +40,109 @@ export const CoreTable = ({
   handleChangeRowsPerPage,
   sx,
   ...rest
-}: CoreTableProps) => (
-  <Paper {...rest} sx={{ ...sx, overflow: 'hidden' }}>
-    <TableContainer>
-      <Table stickyHeader aria-label="sticky table">
-        <TableHead>
-          <TableRow>
-            {columns.map((column) => (
-              <TableCell
-                key={column.field}
-                style={{ minWidth: column.width, backgroundColor: 'white' }}
-              >
-                {column.label}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {(data ?? [])
-            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-            .map((row) => {
-              return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                  {columns.map((column) => {
-                    const value = row[column.field as keyof PlotModel];
-                    return (
-                      <TableCell key={column.field}>
-                        {value as string}
-                      </TableCell>
-                    );
-                  })}
-                </TableRow>
-              );
-            })}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    <TablePagination
-      rowsPerPageOptions={[10, 25, 100]}
-      component="div"
-      count={(data ?? []).length}
-      rowsPerPage={rowsPerPage}
-      page={page}
-      onPageChange={handleChangePage}
-      onRowsPerPageChange={handleChangeRowsPerPage}
-    />
-  </Paper>
-);
+}: CoreTableProps) => {
+  const [left, setLeft] = useState(true);
+  const [right, setRight] = useState(false);
+  const handleScroll = (e: UIEvent<HTMLElement>) => {
+    const isRight =
+      e.currentTarget.scrollWidth - e.currentTarget.scrollLeft ===
+      e.currentTarget.clientWidth;
+    const isLeft =
+      e.currentTarget.scrollWidth - e.currentTarget.scrollLeft ===
+      e.currentTarget.scrollWidth;
+
+    if (isRight && !right) {
+      setRight(true);
+    } else if (right) {
+      setRight(false);
+    }
+    if (isLeft && !left) {
+      setLeft(true);
+    } else if (left) {
+      setLeft(false);
+    }
+  };
+
+  return (
+    <Paper {...rest} sx={{ ...sx, overflowX: 'hidden' }}>
+      <div style={{ position: 'relative' }}>
+        <div
+          style={{
+            boxShadow: `15px 0 12px -12px ${
+              !left ? 'rgba(0,0,0,0.7)' : 'rgba(0,0,0,0)'
+            } inset`,
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            width: 30,
+            zIndex: 99,
+            transition: 'box-shadow .3s',
+          }}
+        />
+        <div
+          style={{
+            boxShadow: `-15px 0 12px -12px ${
+              !right ? 'rgba(0,0,0,0.7)' : 'rgba(0,0,0,0)'
+            } inset`,
+            position: 'absolute',
+            top: 0,
+            bottom: 0,
+            right: 0,
+            width: 30,
+            zIndex: 99,
+            transition: 'box-shadow 0.3s',
+          }}
+        />
+        <div
+          style={{
+            overflowX: 'scroll',
+          }}
+          onScroll={handleScroll}
+        >
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.field}
+                    style={{ minWidth: column.width, backgroundColor: 'white' }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(data ?? [])
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                      {columns.map((column) => {
+                        const value = row[column.field as keyof PlotModel];
+                        return (
+                          <TableCell key={column.field}>
+                            {value as string}
+                          </TableCell>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </div>
+      </div>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={(data ?? []).length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        sx={{ overflow: 'fixed' }}
+      />
+    </Paper>
+  );
+};
