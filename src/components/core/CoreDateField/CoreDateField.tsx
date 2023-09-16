fdc3cf29fb05c59154709b991ec7fc031df986b2
Caden <cnewt2003>
2023-09-16 02:41:46 -0700

    feat(storybook): add toggle button

--- src/components/core/CoreDateField/CoreDateField.tsx
+++ src/components/core/CoreDateField/CoreDateField.tsx
@@ -1,12 +1,27 @@
+import { FormControl } from '@mui/material';
 import type { DatePickerProps } from '@mui/x-date-pickers';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import React from 'react';
 
-export interface CoreDateFieldProps extends DatePickerProps<Date> {}
+import type { CoreInputProps } from '../CoreInput/CoreInput';
+import { CoreInput } from '../CoreInput/CoreInput';
 
-export const CoreDateField = ({ ...rest }: CoreDateFieldProps) => (
-  <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <DatePicker {...rest} />
-  </LocalizationProvider>
+export interface CoreDateFieldProps
+  extends CoreInputProps,
+    DatePickerProps<Date> {}
+
+export const CoreDateField = ({
+  displayLabel,
+  fieldLabel,
+  sx,
+  ...rest
+}: CoreDateFieldProps) => (
+  <CoreInput displayLabel={displayLabel} fieldLabel={fieldLabel}>
+    <FormControl sx={{ minWidth: 200, ...sx }}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker slotProps={{ textField: { size: 'small' } }} {...rest} />
+      </LocalizationProvider>
+    </FormControl>
+  </CoreInput>
 );
