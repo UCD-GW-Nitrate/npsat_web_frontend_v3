b5986f0e9a4039ac4f406d18bf75254b646d4618
Caden <cnewt2003>
2023-09-18 10:31:16 -0700

    feat(step2): render map on step 2

--- src/pages/model/steps/Step2/index.tsx
+++ src/pages/model/steps/Step2/index.tsx
@@ -1,3 +1,4 @@
+import dynamic from 'next/dynamic';
 import React from 'react';
 
 import { CoreForm } from '@/components/core/CoreForm/CoreForm';
@@ -9,13 +10,23 @@ import type { Step } from '../../create';
 interface Step2Props extends Step {}
 
 const Step2 = ({ onPrev, onNext }: Step2Props) => {
+  const MapWithNoSSR = dynamic(
+    () => import('@/components/maps/FormMapSelect'),
+    {
+      ssr: false,
+    },
+  );
+
   return (
     <CoreForm
-      fields={[{ label: 'Advanced filter:' }]}
+      fields={[{ label: '' }, { label: 'Advanced filter:' }]}
       sx={{
         mt: 6,
       }}
     >
+      <div id="map" style={{ height: '600px', width: '500px' }}>
+        <MapWithNoSSR mapType="valley" />
+      </div>
       <CoreSwitch />
       <PageAdvancementButtons onClickPrev={onPrev} onClickNext={onNext} />
     </CoreForm>
