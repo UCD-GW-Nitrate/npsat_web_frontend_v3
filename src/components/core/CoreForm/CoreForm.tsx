16a84a0d04d219f0fe9044a177d589139b695441
Caden <cnewt2003>
2023-09-18 08:14:58 -0700

    feat(step1): add form

---
+++ src/components/core/CoreForm/CoreForm.tsx
@@ -0,0 +1,46 @@
+import type { BoxProps } from '@mui/material';
+import { Box, Stack } from '@mui/material';
+import React, { Children } from 'react';
+
+import { HBox } from '@/components/custom/HBox/Hbox';
+
+import { CoreText } from '../CoreText/CoreText';
+
+export interface CoreFormField {
+  label: string;
+  required?: boolean;
+  errorMessage?: string;
+}
+
+export interface CoreFormProps extends BoxProps {
+  fields: CoreFormField[];
+}
+
+export const CoreForm = ({ children, fields, ...rest }: CoreFormProps) => {
+  const childrenArray = Children.toArray(children);
+  for (let i = 0; i < childrenArray.length - fields.length; i += 1) {
+    fields.push({ label: '' } as CoreFormField);
+  }
+
+  return (
+    <Box component="form" {...rest}>
+      <Stack spacing={4} alignItems="flex-start" flexDirection="column">
+        {childrenArray.map((child, index) => {
+          return (
+            <HBox key={fields[index]?.label ?? ''} spacing={2}>
+              <Stack
+                flexDirection="column"
+                sx={{ width: '30vw', overflow: 'hidden' }}
+              >
+                <CoreText sx={{ marginLeft: 'auto' }} noWrap>
+                  {fields[index]?.label ?? ''}
+                </CoreText>
+              </Stack>
+              {child}
+            </HBox>
+          );
+        })}
+      </Stack>
+    </Box>
+  );
+};
--- src/components/core/CoreSelect/CoreSelect.tsx
+++ src/components/core/CoreSelect/CoreSelect.tsx
@@ -1,28 +1,17 @@
-import type { FormControlProps, SelectChangeEvent } from '@mui/material';
-import { FormControl, MenuItem, Select } from '@mui/material';
+import type { BoxProps, SelectChangeEvent } from '@mui/material';
+import { Box, MenuItem, Select } from '@mui/material';
 import React from 'react';
-
-import type { CoreInputProps } from '../CoreInput/CoreInput';
-import { CoreInput } from '../CoreInput/CoreInput';
 
 export interface CoreSelectOption {
   label: string;
   value?: any | undefined;
 }
 
-export interface CoreSelectProps extends CoreInputProps, FormControlProps {
+export interface CoreSelectProps extends BoxProps {
   options: CoreSelectOption[];
 }
 
-export const CoreSelect = ({
-  options,
-  fieldLabel,
-  displayLabel,
-  labelStyle,
-  fullWidth,
-  sx,
-  ...rest
-}: CoreSelectProps) => {
+export const CoreSelect = ({ options, sx, ...rest }: CoreSelectProps) => {
   const [inputValue, setInputValue] = React.useState('');
 
   const handleChange = (event: SelectChangeEvent<unknown>) => {
@@ -30,26 +19,20 @@ export const CoreSelect = ({
   };
 
   return (
-    <CoreInput
-      displayLabel={displayLabel}
-      fieldLabel={fieldLabel}
-      labelStyle={labelStyle}
-      fullWidth={fullWidth}
-    >
-      <FormControl sx={{ margin: 'auto', ...sx }} {...rest}>
-        <Select
-          value={inputValue}
-          onChange={handleChange}
-          displayEmpty
-          size="small"
-        >
-          {options.map(({ label, value }: CoreSelectOption) => (
-            <MenuItem key={label} value={value ?? label}>
-              {label}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </CoreInput>
+    <Box sx={{ width: 200, ...sx }} {...rest}>
+      <Select
+        value={inputValue}
+        onChange={handleChange}
+        displayEmpty
+        size="small"
+        sx={{ width: 200, ...sx }}
+      >
+        {options.map(({ label, value }: CoreSelectOption) => (
+          <MenuItem key={label} value={value ?? label}>
+            {label}
+          </MenuItem>
+        ))}
+      </Select>
+    </Box>
   );
 };
