8eaf0261bf47f57db6603922ad3db04f876198ea
Caden <cnewt2003>
2023-09-18 10:46:39 -0700

    feat(step2): change map with tab press

--- src/pages/model/steps/Step2/index.tsx
+++ src/pages/model/steps/Step2/index.tsx
@@ -1,15 +1,30 @@
+import { Box } from '@mui/material';
 import dynamic from 'next/dynamic';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { CoreForm } from '@/components/core/CoreForm/CoreForm';
 import { CoreSwitch } from '@/components/core/CoreSwitch/CoreSwitch';
+import { CoreTabs } from '@/components/core/CoreTabs/CoreTabs';
 import { PageAdvancementButtons } from '@/components/custom/PageAdvancementButtons/PageAdvancementButtons';
 
 import type { Step } from '../../create';
 
 interface Step2Props extends Step {}
 
+const tabs = [
+  { label: 'Central Valley', value: 'valley' },
+  { label: 'Basin', value: 'basin' },
+  { label: 'County', value: 'county' },
+  { label: 'B118 Basin', value: 'b118basin' },
+  { label: 'Subregions', value: 'subregions' },
+  { label: 'Township', value: 'township' },
+];
+
 const Step2 = ({ onPrev, onNext }: Step2Props) => {
+  const [mapType, setMapType] = useState<
+    'valley' | 'basin' | 'county' | 'b118basin' | 'subregions' | 'township'
+  >('valley');
+
   const MapWithNoSSR = dynamic(
     () => import('@/components/maps/FormMapSelect'),
     {
@@ -17,19 +32,34 @@ const Step2 = ({ onPrev, onNext }: Step2Props) => {
     },
   );
 
+  const handleTabChange = (tab: string) => {
+    setMapType(
+      tab as
+        | 'valley'
+        | 'basin'
+        | 'county'
+        | 'b118basin'
+        | 'subregions'
+        | 'township',
+    );
+  };
+
   return (
-    <CoreForm
-      fields={[{ label: '' }, { label: 'Advanced filter:' }]}
-      sx={{
-        mt: 6,
-      }}
-    >
-      <div id="map" style={{ height: '600px', width: '500px' }}>
-        <MapWithNoSSR mapType="valley" />
-      </div>
-      <CoreSwitch />
-      <PageAdvancementButtons onClickPrev={onPrev} onClickNext={onNext} />
-    </CoreForm>
+    <Box sx={{ mt: 6 }}>
+      <CoreTabs tabs={tabs} onTabChange={handleTabChange} />
+      <CoreForm
+        fields={[{ label: '' }, { label: 'Advanced filter:' }]}
+        sx={{
+          mt: 6,
+        }}
+      >
+        <div id="map" style={{ height: '600px', width: '500px' }}>
+          <MapWithNoSSR mapType={mapType} />
+        </div>
+        <CoreSwitch />
+        <PageAdvancementButtons onClickPrev={onPrev} onClickNext={onNext} />
+      </CoreForm>
+    </Box>
   );
 };
 
