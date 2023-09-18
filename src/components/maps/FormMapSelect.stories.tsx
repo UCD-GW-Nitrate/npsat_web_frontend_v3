b16e4aaf424ba6febb38d5ce4063b1bd71b0b430
Caden Newton <cnewton@ucdavis.edu>
2023-09-18 03:27:30 -0700

    feat(storybook): add counties to map

---
+++ src/components/maps/FormMapSelect.stories.tsx
@@ -0,0 +1,28 @@
+import { ThemeProvider } from '@emotion/react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { Provider } from 'react-redux';
+
+import theme from '@/components/theme';
+import { store } from '@/store';
+
+import type { FormMapSelectProps } from './FormMapSelect';
+import { FormMapSelect } from './FormMapSelect';
+
+const meta: Meta<typeof FormMapSelect> = {
+  component: FormMapSelect,
+};
+
+export default meta;
+type Story = StoryObj<typeof FormMapSelect>;
+
+export const Primary: Story = {
+  render: ({ ...args }: FormMapSelectProps) => (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <div id="map" style={{ height: '600px' }}>
+          <FormMapSelect {...args} />
+        </div>
+      </ThemeProvider>
+    </Provider>
+  ),
+};
