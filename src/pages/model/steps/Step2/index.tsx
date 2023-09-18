a62cf0ed0af6a24709b616f9803d7c9e2b232113
Caden Newton <cnewton@ucdavis.edu>
2023-09-18 09:28:28 -0700

    feat(steps): handle next and prev

--- src/pages/model/steps/Step2/index.tsx
+++ src/pages/model/steps/Step2/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
-const Step2 = () => {
-  return <div />;
+import { CoreForm } from '@/components/core/CoreForm/CoreForm';
+import { CoreSwitch } from '@/components/core/CoreSwitch/CoreSwitch';
+import { PageAdvancementButtons } from '@/components/custom/PageAdvancementButtons/PageAdvancementButtons';
+
+import type { Step } from '../../create';
+
+interface Step2Props extends Step {}
+
+const Step2 = ({ onPrev, onNext }: Step2Props) => {
+  return (
+    <CoreForm
+      fields={[{ label: 'Advanced filter:' }]}
+      sx={{
+        mt: 6,
+      }}
+    >
+      <CoreSwitch />
+      <PageAdvancementButtons onClickPrev={onPrev} onClickNext={onNext} />
+    </CoreForm>
+  );
 };
 
 export default Step2;
--- src/pages/model/steps/Step4/index.tsx
+++ src/pages/model/steps/Step4/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-const Step4 = () => {
-  return <div />;
+import { CoreForm } from '@/components/core/CoreForm/CoreForm';
+import { PageAdvancementButtons } from '@/components/custom/PageAdvancementButtons/PageAdvancementButtons';
+
+import type { Step } from '../../create';
+
+interface Step4Props extends Step {}
+
+const Step4 = ({ onPrev, onNext }: Step4Props) => {
+  return (
+    <CoreForm
+      fields={[]}
+      sx={{
+        mt: 6,
+      }}
+    >
+      <PageAdvancementButtons onClickPrev={onPrev} onClickNext={onNext} />
+    </CoreForm>
+  );
 };
 
 export default Step4;
