8cf6b78eaa2ab250e4eeef6ddc5c8f9bc56c1afc
Caden <cnewt2003>
2023-09-18 11:24:10 -0700

    feat(step4): add step 4 components

--- src/pages/model/steps/Step4/index.tsx
+++ src/pages/model/steps/Step4/index.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 
 import { CoreForm } from '@/components/core/CoreForm/CoreForm';
+import { CoreTextField } from '@/components/core/CoreTextField/CoreTextField';
 import { PageAdvancementButtons } from '@/components/custom/PageAdvancementButtons/PageAdvancementButtons';
 
 import type { Step } from '../../create';
 
 interface Step4Props extends Step {}
 
+const fields = [
+  { label: 'Scenario name' },
+  {
+    label: 'Description',
+    position: 'flex-start' as 'flex-start' | 'center' | 'flex-end',
+  },
+];
+
 const Step4 = ({ onPrev, onNext }: Step4Props) => {
   return (
     <CoreForm
-      fields={[]}
+      fields={fields}
       sx={{
         mt: 6,
       }}
     >
+      <CoreTextField sx={{ width: 400 }} />
+      <CoreTextField sx={{ width: 400 }} multiline />
       <PageAdvancementButtons onClickPrev={onPrev} onClickNext={onNext} />
     </CoreForm>
   );
