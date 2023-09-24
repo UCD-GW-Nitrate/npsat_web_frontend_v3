bcc8ade0db4051ed4f9d5c33960946bb56697591
Caden <cnewt2003>
2023-09-23 17:03:55 -0700

    feat(step1): extract data from form

--- src/pages/model/steps/Step1/index.tsx
+++ src/pages/model/steps/Step1/index.tsx
@@ -1,5 +1,7 @@
 import { Box, Divider } from '@mui/material';
+import type { FormEvent } from 'react';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { CoreDateField } from '@/components/core/CoreDateField/CoreDateField';
 import { CoreDateRangeField } from '@/components/core/CoreDateRangeField/CoreDateRangeField';
@@ -8,6 +10,7 @@ import { CoreNumberField } from '@/components/core/CoreNumberField/CoreNumberFie
 import { CoreSelect } from '@/components/core/CoreSelect/CoreSelect';
 import { CoreToggleButton } from '@/components/core/CoreToggleButton/CoreToggleButton';
 import { PageAdvancementButtons } from '@/components/custom/PageAdvancementButtons/PageAdvancementButtons';
+import { saveCurrentStep } from '@/store/slices/modelSlice';
 
 import type { Step } from '../../create';
 import Step1Instructions from './Step1Instructions';
@@ -16,14 +19,14 @@ const flowScenarioOptions = [
   { label: 'C2Vsim' },
   { label: 'CVHM (currently unavailable)' },
 ];
-const loadScenario = [
+const loadScenarioOptions = [
   { label: 'GNLM' },
   { label: 'Baseline' },
   { label: 'High Fertilization' },
   { label: 'High Irrigation' },
   { label: 'High Irrigation and High Fertilization' },
 ];
-const wellTypeScenario = [
+const wellTypeScenarioOptions = [
   { label: 'Public Supply Wells and Irrigation Wells' },
   { label: 'Domestic Wells' },
   {
@@ -31,7 +34,7 @@ const wellTypeScenario = [
       'Virtual Shallow Monitoring Well Network Grid (currently unavailable)',
   },
 ];
-const unsatZoneDepthScenario = [
+const unsatZoneDepthScenarioOptions = [
   { label: 'Drought vadose zone thickness (spring 2015)' },
   { label: 'Typical vadose zone thickness (spring 2000)' },
 ];
@@ -53,6 +56,29 @@ const fields = [
 interface Step1Props extends Step {}
 
 const Step1 = ({ onPrev, onNext }: Step1Props) => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const flowScenario: string = data.get('flow scenario')?.toString() ?? '';
+    const loadScenario: string = data.get('load scenario')?.toString() ?? '';
+    const wellTypeScenario: string =
+      data.get('well type scenario')?.toString() ?? '';
+    const unsatZoneDepthScenario: string =
+      data.get('unsat zone depth scenario')?.toString() ?? '';
+    dispatch(
+      saveCurrentStep({
+        flow_scenario: flowScenario,
+        load_scenario: loadScenario,
+        unsat_scenario: unsatZoneDepthScenario,
+        welltype_scenario: wellTypeScenario,
+      }),
+    );
+
+    onNext();
+  };
+
   return (
     <Box>
       <CoreForm
@@ -60,28 +86,37 @@ const Step1 = ({ onPrev, onNext }: Step1Props) => {
         sx={{
           mt: 6,
         }}
+        onFormSubmit={handleSubmit}
       >
         <CoreSelect
           options={flowScenarioOptions}
           sx={{ display: 'flex', flexGrow: 1 }}
+          name="flow scenario"
+          key="flow scenario"
         />
         <CoreSelect
-          options={loadScenario}
+          options={loadScenarioOptions}
           sx={{ display: 'flex', flexGrow: 1 }}
+          name="load scenario"
+          key="load scenario"
         />
         <CoreSelect
-          options={wellTypeScenario}
+          options={wellTypeScenarioOptions}
           sx={{ display: 'flex', flexGrow: 1 }}
+          name="well type scenario"
+          key="well type scenario"
         />
         <CoreSelect
-          options={unsatZoneDepthScenario}
+          options={unsatZoneDepthScenarioOptions}
           sx={{ display: 'flex', flexGrow: 1 }}
+          name="unsat zone depth scenario"
+          key="unsat zone depth scenario"
         />
-        <CoreNumberField sx={{ width: 100 }} units="%" />
+        <CoreNumberField sx={{ width: 100 }} units="%" name="water content" />
         <CoreDateField />
         <CoreToggleButton options={transiptionPeriodOptions} />
         <CoreDateRangeField />
-        <PageAdvancementButtons onClickPrev={onPrev} onClickNext={onNext} />
+        <PageAdvancementButtons onClickPrev={onPrev} />
       </CoreForm>
       <Divider sx={{ mt: 6 }} />
       <Step1Instructions />
