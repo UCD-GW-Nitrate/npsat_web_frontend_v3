e805e3918f5c5f1cafee5278b319b0aecdd18b8f
Caden <cnewt2003>
2023-09-25 10:01:07 -0700

    feat(step2): save form data

--- src/components/core/CoreRangeSlider/CoreRangeSlider.tsx
+++ src/components/core/CoreRangeSlider/CoreRangeSlider.tsx
@@ -1,84 +1,77 @@
-import type { SliderProps } from '@mui/material';
+import type { BoxProps } from '@mui/material';
 import { Box, Slider } from '@mui/material';
 import React, { useState } from 'react';
+import { Controller } from 'react-hook-form';
 
 import { HBox } from '@/components/custom/HBox/Hbox';
 
-import type { CoreInputProps } from '../CoreInput/CoreInput';
 import { CoreInput } from '../CoreInput/CoreInput';
 import { CoreNumberField } from '../CoreNumberField/CoreNumberField';
 
-export interface CoreRangeSliderProps extends CoreInputProps, SliderProps {
+export interface CoreRangeSliderProps extends BoxProps {
   units?: string;
   minFieldLabel?: string;
   maxFieldLabel?: string;
+  name?: string;
 }
 
 export const CoreRangeSlider = ({
-  displayLabel,
-  fieldLabel,
-  labelStyle,
-  fullWidth,
   units,
   minFieldLabel,
   maxFieldLabel,
+  name,
   ...rest
 }: CoreRangeSliderProps) => {
   const [range, setRange] = useState<number[]>([0, 100]);
 
-  const handleMinNumberChange = (value: number) => {
-    setRange([value, range[1] as number]);
-  };
-
-  const handleMaxNumberChange = (value: number) => {
-    setRange([range[0] as number, value]);
-  };
-
-  const handleChange = (_event: Event, newValue: number | number[]) => {
-    setRange(newValue as number[]);
-  };
-
   return (
-    <Box>
-      <CoreInput
-        displayLabel={displayLabel}
-        fieldLabel={fieldLabel}
-        labelStyle={labelStyle}
-        fullWidth={fullWidth}
-      >
-        <HBox spacing={2}>
-          <Slider
-            sx={{ width: 150 }}
-            {...rest}
-            value={range}
-            onChange={handleChange}
-          />
-          <div>
-            <CoreInput
-              displayLabel="left"
-              fieldLabel={minFieldLabel}
-              sx={{ ml: 2 }}
-            >
-              <CoreNumberField
-                value={range[0]}
-                onNumberChange={handleMinNumberChange}
-                units={units}
-                sx={{ width: 90 }}
-              />
-            </CoreInput>
-          </div>
-          <div>
-            <CoreInput displayLabel="left" fieldLabel={maxFieldLabel}>
-              <CoreNumberField
-                value={range[1]}
-                onNumberChange={handleMaxNumberChange}
-                units={units}
-                sx={{ width: 90 }}
-              />
-            </CoreInput>
-          </div>
-        </HBox>
-      </CoreInput>
+    <Box {...rest}>
+      <Controller
+        name={name ?? ''}
+        defaultValue={range}
+        render={({ field: { onChange } }) => (
+          <HBox spacing={2}>
+            <Slider
+              sx={{ width: 150 }}
+              value={range}
+              onChange={(_event: Event, newValue: number | number[]) => {
+                onChange(newValue as number[]);
+                setRange(newValue as number[]);
+              }}
+            />
+            <div>
+              <CoreInput
+                displayLabel="left"
+                fieldLabel={minFieldLabel}
+                sx={{ ml: 2 }}
+              >
+                <CoreNumberField
+                  value={range[0]}
+                  onNumberChange={(value: number) => {
+                    setRange([value, range[1] as number]);
+                    onChange([value, range[1] as number]);
+                  }}
+                  units={units}
+                  sx={{ width: 90 }}
+                />
+              </CoreInput>
+            </div>
+            <div>
+              <CoreInput displayLabel="left" fieldLabel={maxFieldLabel}>
+                <CoreNumberField
+                  value={range[1]}
+                  onNumberChange={(value: number) => {
+                    setRange([range[0] as number, value]);
+                    onChange([range[0] as number, value]);
+                  }}
+                  units={units}
+                  sx={{ width: 90 }}
+                />
+              </CoreInput>
+            </div>
+          </HBox>
+        )}
+      />
     </Box>
   );
 };
