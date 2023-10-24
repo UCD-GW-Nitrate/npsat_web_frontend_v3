108276208414e399fae1a225b93d77aafed49a56
Caden Newton <cnewton@ucdavis.edu>
2023-10-24 06:53:50 -0700

    feat(homepage): navigate to model

---
+++ src/pages/model/[modelId]/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '@/components/custom/Layout/Layout';
+
+const ModelPage = () => {
+  return (
+    <Layout>
+      <div />
+    </Layout>
+  );
+};
+
+export default ModelPage;
