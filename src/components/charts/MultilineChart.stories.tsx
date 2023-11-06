eb86dc25c40fc8f2adb044f0e2783d48341f87aa
Caden <cnewt2003>
2023-11-06 01:13:01 -0800

    feat(graphs): add annotation

---
+++ src/components/charts/MultilineChart.stories.tsx
@@ -0,0 +1,100 @@
+import { ThemeProvider } from '@emotion/react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import theme from '@/components/theme';
+
+import type { MultilineChartProps } from './MultilineChart';
+import MultilineChart from './MultilineChart';
+
+const meta: Meta<typeof MultilineChart> = {
+  component: MultilineChart,
+};
+
+export default meta;
+type Story = StoryObj<typeof MultilineChart>;
+
+export const Primary: Story = {
+  render: ({ ...args }: MultilineChartProps) => (
+    <ThemeProvider theme={theme}>
+      <MultilineChart {...args} />
+    </ThemeProvider>
+  ),
+  args: {
+    width: 800,
+    height: 400,
+    chartType: 'line',
+    yLabel: 'Concentration of Nitrate as N [mg/L]',
+    min: 2000,
+    max: 2007,
+    data: {
+      custom: [
+        {
+          year: 2000,
+          value: 10,
+        },
+        {
+          year: 2001,
+          value: 13,
+        },
+        {
+          year: 2002,
+          value: 14,
+        },
+        {
+          year: 2003,
+          value: 10,
+        },
+        {
+          year: 2004,
+          value: 16,
+        },
+        {
+          year: 2005,
+          value: 9,
+        },
+        {
+          year: 2006,
+          value: 7,
+        },
+        {
+          year: 2007,
+          value: 11,
+        },
+      ],
+      base: [
+        {
+          year: 2000,
+          value: 9,
+        },
+        {
+          year: 2001,
+          value: 14,
+        },
+        {
+          year: 2002,
+          value: 16,
+        },
+        {
+          year: 2003,
+          value: 17,
+        },
+        {
+          year: 2004,
+          value: 16,
+        },
+        {
+          year: 2005,
+          value: 14,
+        },
+        {
+          year: 2006,
+          value: 16,
+        },
+        {
+          year: 2007,
+          value: 18,
+        },
+      ],
+    },
+  },
+};
