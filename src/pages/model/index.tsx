be8e79ef8c9019c49616d70b9cfeae3154ee977e
Caden Newton <cnewton@ucdavis.edu>
2023-12-04 10:05:24 -0800

    feat(graphs): plot with predefined percentiles

--- src/pages/model/index.tsx
+++ src/pages/model/index.tsx
@@ -1,9 +1,11 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import type { ChartDataPoint } from '@/components/charts/MultilinePlot/MultilineChartBase';
 import MultilineChartBase from '@/components/charts/MultilinePlot/MultilineChartBase';
+import { CoreButton } from '@/components/core/CoreButton/CoreButton';
 import { CoreContainer } from '@/components/core/CoreContainer/CoreContainer';
+import { HBox } from '@/components/custom/HBox/Hbox';
 import Layout from '@/components/custom/Layout/Layout';
 import type { ModelDisplay } from '@/hooks/useModelResults';
 import { useModelResults } from '@/hooks/useModelResults';
@@ -14,12 +16,46 @@ interface PlotWithPercentilesProps {
   percentiles: Result[];
 }
 
+interface DisplayData {
+  [percentile: string]: ChartDataPoint[];
+}
+
 const PlotWithPercentiles = ({ percentiles }: PlotWithPercentilesProps) => {
   const [plotData] = useModelResults(percentiles);
+  const [percentilesDisplayed, setPercentilesDisplayed] = useState<number[]>([
+    5, 50, 95,
+  ]);
+
+  function configureDisplayData(percentilesInput: number[]) {
+    const res: any = {};
+    percentilesInput.forEach((p) => {
+      res[`${p}th percentile`] =
+        ((plotData as any)[p] as ModelDisplay[] | undefined)?.map((data) => {
+          return {
+            year: data.year,
+            value: data.value,
+          } as ChartDataPoint;
+        }) ?? [];
+    });
+    return res;
+  }
+
+  const [displayData, setDisplayData] = useState<DisplayData>(
+    configureDisplayData(percentilesDisplayed),
+  );
+
+  useEffect(() => {
+    setDisplayData(configureDisplayData(percentilesDisplayed));
+  }, [plotData, percentilesDisplayed]);
 
   if (!plotData) {
     return <div />;
   }
+
+  console.log('plot data');
+  console.log(plotData);
+  console.log('display data');
+  console.log(displayData);
 
   return (
     <CoreContainer
@@ -28,53 +64,42 @@ const PlotWithPercentiles = ({ percentiles }: PlotWithPercentilesProps) => {
         backgroundColor: 'white',
         display: 'flex',
         justifyContent: 'center',
+        flexDirection: 'column',
       }}
     >
+      <HBox spacing={1} sx={{ mt: 4, ml: 5 }}>
+        <CoreButton
+          variant="contained"
+          label="Select 5th, 50th, 95th"
+          onClick={() => setPercentilesDisplayed([5, 50, 95])}
+        />
+        <CoreButton
+          variant="contained"
+          label="Select 10th, 50th, 90th"
+          onClick={() => setPercentilesDisplayed([10, 50, 90])}
+        />
+      </HBox>
       <MultilineChartBase
         height={500}
         chartType="line"
         yLabel="Concentration of Nitrate as N [mg/L]"
         annotations={[
           {
-            dataKey: '90th percentile',
+            dataKey: `${
+              percentilesDisplayed[percentilesDisplayed.length - 1]
+            }th percentile`,
             index: 70,
             title: 'Implementation start year',
           },
           {
-            dataKey: '90th percentile',
+            dataKey: `${
+              percentilesDisplayed[percentilesDisplayed.length - 1]
+            }th percentile`,
             index: 75,
             title: 'Implementation complete year',
           },
         ]}
-        data={{
-          '10th percentile':
-            ((plotData as any)[10] as ModelDisplay[] | undefined)?.map(
-              (data) => {
-                return {
-                  year: data.year,
-                  value: data.value,
-                } as ChartDataPoint;
-              },
-            ) ?? [],
-          '50th percentile':
-            ((plotData as any)[50] as ModelDisplay[] | undefined)?.map(
-              (data) => {
-                return {
-                  year: data.year,
-                  value: data.value,
-                } as ChartDataPoint;
-              },
-            ) ?? [],
-          '90th percentile':
-            ((plotData as any)[90] as ModelDisplay[] | undefined)?.map(
-              (data) => {
-                return {
-                  year: data.year,
-                  value: data.value,
-                } as ChartDataPoint;
-              },
-            ) ?? [],
-        }}
+        data={displayData}
       />
     </CoreContainer>
   );
