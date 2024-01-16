593ffb95636c3fb8b1cec850bde422c2782d35e9
Caden <cnewt2003>
2024-01-15 18:04:11 -0800

    feat(compare): checkboxes to compare model

--- src/hooks/useModelResults.ts
+++ src/hooks/useModelResults.ts
@@ -20,6 +20,45 @@ export interface PercentileResultMap {
 const ordinalSuffix = (num: number) =>
   `${num}${['st', 'nd', 'rd'][((((num + 90) % 100) - 10) % 10) - 1] || 'th'}`;
 
+const getModelResult = (
+  percentiles: Result[],
+  authToken: string | null,
+  callback: (
+    percentilesInput: number[],
+    plotInput: PercentileResultMap,
+  ) => void,
+) => {
+  Promise.all(
+    percentiles.map((percentile) =>
+      axios.get<ModelResult>(
+        `http://localhost:8010/api/model_result/${percentile.id}/`,
+        {
+          headers: {
+            Authorization: `Token ${authToken}`,
+          },
+        },
+      ),
+    ),
+  ).then((data) => {
+    const results = {};
+    data.forEach((percentile) => {
+      const res: ModelDisplay[] = percentile.data.values.map((value, index) =>
+        // start year is always 1945
+        ({
+          year: 1945 + index,
+          value,
+          percentile: `${ordinalSuffix(percentile.data.percentile)} percentile`,
+        }),
+      );
+      Object.assign(results, { [`${percentile.data.percentile}`]: res });
+    });
+    callback(
+      data.map((percentile) => percentile.data.percentile),
+      results,
+    );
+  });
+};
+
 export const useModelResults = (
   percentiles: Result[],
 ): [PercentileResultMap, number[]] => {
@@ -31,45 +70,43 @@ export const useModelResults = (
 
   useEffect(() => {
     if (percentiles) {
-      Promise.all(
-        percentiles.map((percentile) =>
-          axios.get<ModelResult>(
-            `http://localhost:8010/api/model_result/${percentile.id}/`,
-            {
-              headers: {
-                Authorization: `Token ${auth.token}`,
-              },
-            },
-          ),
-        ),
-      ).then((data) => {
-        const results = {};
-        data.forEach((percentile) => {
-          const res: ModelDisplay[] = percentile.data.values.map(
-            (value, index) =>
-              // start year is always 1945
-              ({
-                year: 1945 + index,
-                value,
-                percentile: `${ordinalSuffix(
-                  percentile.data.percentile,
-                )} percentile`,
-              }),
-          );
-          Object.assign(results, { [`${percentile.data.percentile}`]: res });
-        });
-        setPercentilesData(
-          data.map((percentile) => percentile.data.percentile),
-        );
-        setData(results);
-      });
+      getModelResult(
+        percentiles,
+        auth.token,
+        (percentilesInput: number[], plotInput: PercentileResultMap) => {
+          setPercentilesData(percentilesInput);
+          setData(plotInput);
+        },
+      );
     }
   }, [percentiles]);
 
-  console.log('model res:');
-  console.log(plotData);
-  console.log('percentiles:');
-  console.log(percentilesData);
+  return [plotData, percentilesData];
+};
+
+export const useAllModelResults = (
+  modelResults: Result[][],
+): [PercentileResultMap[], number[]] => {
+  const [plotData, setPlotData] = useState<PercentileResultMap[]>([]);
+  const [percentilesData, setPercentilesData] = useState<number[]>([]);
+  const auth = useSelector<RootState, AuthState>((state) => {
+    return state.auth;
+  });
+
+  useEffect(() => {
+    const plotDataResult: PercentileResultMap[] = [];
+    modelResults.forEach((percentiles) => {
+      getModelResult(
+        percentiles,
+        auth.token,
+        (percentilesInput: number[], plotInput: PercentileResultMap) => {
+          setPercentilesData(percentilesInput);
+          plotDataResult.push(plotInput);
+        },
+      );
+    });
+    setPlotData(plotDataResult);
+  }, [modelResults]);
 
   return [plotData, percentilesData];
 };
