4b53ba12b90865a5aa99b9a70f1e155d9aabb477
Caden <cnewt2003>
2024-01-16 00:40:03 -0800

    feat(compare): add compare selected table

--- src/components/core/CoreTable/CoreTable.tsx
+++ src/components/core/CoreTable/CoreTable.tsx
@@ -9,9 +9,9 @@ import {
   TableRow,
 } from '@mui/material';
 import { useRouter } from 'next/router';
-import type { UIEvent } from 'react';
 import React, { useState } from 'react';
 
+import Scrollable from '@/components/custom/Scrollable/Scrollable';
 import type { PlotModel } from '@/store/apis/feedApi';
 
 import { CoreContainer } from '../CoreContainer/CoreContainer';
@@ -49,8 +49,6 @@ export const CoreTable = ({
   ...rest
 }: CoreTableProps) => {
   const [selected, setSelected] = useState<number[]>([]);
-  const [left, setLeft] = useState(true);
-  const [right, setRight] = useState(false);
   const router = useRouter();
 
   const isSelected = (id: number) => {
@@ -60,26 +58,6 @@ export const CoreTable = ({
       }
     }
     return false;
-  };
-
-  const handleScroll = (e: UIEvent<HTMLElement>) => {
-    const isRight =
-      e.currentTarget.scrollWidth - e.currentTarget.scrollLeft ===
-      e.currentTarget.clientWidth;
-    const isLeft =
-      e.currentTarget.scrollWidth - e.currentTarget.scrollLeft ===
-      e.currentTarget.scrollWidth;
-
-    if (isRight && !right) {
-      setRight(true);
-    } else if (right) {
-      setRight(false);
-    }
-    if (isLeft && !left) {
-      setLeft(true);
-    } else if (left) {
-      setLeft(false);
-    }
   };
 
   const handleCheckboxClick = (
@@ -119,107 +97,73 @@ export const CoreTable = ({
 
   return (
     <CoreContainer {...rest} sx={{ ...sx, overflowX: 'hidden', p: 0 }}>
-      <div style={{ position: 'relative' }}>
-        <div
-          style={{
-            boxShadow: `15px 0 12px -12px ${
-              !left ? 'rgba(0,0,0,0.4)' : 'rgba(0,0,0,0)'
-            } inset`,
-            position: 'absolute',
-            top: 0,
-            bottom: 0,
-            width: 30,
-            zIndex: 99,
-            transition: 'box-shadow .3s',
-          }}
-        />
-        <div
-          style={{
-            boxShadow: `-15px 0 12px -12px ${
-              !right ? 'rgba(0,0,0,0.4)' : 'rgba(0,0,0,0)'
-            } inset`,
-            position: 'absolute',
-            top: 0,
-            bottom: 0,
-            right: 0,
-            width: 30,
-            zIndex: 99,
-            transition: 'box-shadow 0.3s',
-          }}
-        />
-        <div
-          style={{
-            overflowX: 'scroll',
-          }}
-          onScroll={handleScroll}
-        >
-          <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-              <TableRow>
-                {checkboxSelection && (
-                  <TableCell>
-                    <Checkbox
-                      indeterminate={
-                        selected.length > 0 && selected.length < data.length
-                      }
-                      checked={selected.length === data.length}
-                      onChange={handleSelectAllClick}
-                    />
-                  </TableCell>
-                )}
-                {columns.map((column) => (
-                  <TableCell
-                    key={column.field}
-                    style={{ minWidth: column.width, backgroundColor: 'white' }}
+      <Scrollable>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {checkboxSelection && (
+                <TableCell>
+                  <Checkbox
+                    indeterminate={
+                      selected.length > 0 && selected.length < data.length
+                    }
+                    checked={selected.length === data.length}
+                    onChange={handleSelectAllClick}
+                  />
+                </TableCell>
+              )}
+              {columns.map((column) => (
+                <TableCell
+                  key={column.field}
+                  style={{ minWidth: column.width, backgroundColor: 'white' }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(data ?? [])
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={row.id}
+                    onClick={() =>
+                      router.push({
+                        pathname: `/model/`,
+                        query: { id: row.id },
+                      })
+                    }
+                    selected={isSelected(row.id)}
                   >
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {(data ?? [])
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
-                  return (
-                    <TableRow
-                      hover
-                      role="checkbox"
-                      tabIndex={-1}
-                      key={row.id}
-                      onClick={() =>
-                        router.push({
-                          pathname: `/model/`,
-                          query: { id: row.id },
-                        })
-                      }
-                      selected={isSelected(row.id)}
-                    >
-                      {checkboxSelection && (
-                        <TableCell sx={{ py: 0 }}>
-                          <Checkbox
-                            onClick={(event) =>
-                              handleCheckboxClick(event, row.id)
-                            }
-                            checked={isSelected(row.id)}
-                          />
+                    {checkboxSelection && (
+                      <TableCell sx={{ py: 0 }}>
+                        <Checkbox
+                          onClick={(event) =>
+                            handleCheckboxClick(event, row.id)
+                          }
+                          checked={isSelected(row.id)}
+                        />
+                      </TableCell>
+                    )}
+                    {columns.map((column) => {
+                      const value = row[column.field as keyof PlotModel];
+                      return (
+                        <TableCell key={column.field}>
+                          {value as string}
                         </TableCell>
-                      )}
-                      {columns.map((column) => {
-                        const value = row[column.field as keyof PlotModel];
-                        return (
-                          <TableCell key={column.field}>
-                            {value as string}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  );
-                })}
-            </TableBody>
-          </Table>
-        </div>
-      </div>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </Scrollable>
       <TablePagination
         rowsPerPageOptions={[10, 25, 100]}
         component="div"
