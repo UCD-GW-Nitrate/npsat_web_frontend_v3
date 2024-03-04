9638a2d4b314398477deee914bb2c97212a51e76
Caden Newton <cnewton@ucdavis.edu>
2024-03-03 21:20:12 -0800

    feat(mantis): post model request works

--- src/components/core/CoreSwitch/CoreSwitch.tsx
+++ src/components/core/CoreSwitch/CoreSwitch.tsx
@@ -1,24 +1,37 @@
 import type { BoxProps } from '@mui/material';
 import { Box, Switch } from '@mui/material';
 import React from 'react';
+import { Controller } from 'react-hook-form';
 
 export interface CoreSwitchProps extends BoxProps {
   onSwitchChange?: (value: boolean) => void;
+  name?: string;
 }
 
-export const CoreSwitch = ({ onSwitchChange, ...rest }: CoreSwitchProps) => {
-  const handleChange = (
-    _event: React.ChangeEvent<HTMLInputElement>,
-    checked: boolean,
-  ) => {
-    if (onSwitchChange) {
-      onSwitchChange(checked);
-    }
-  };
-
+export const CoreSwitch = ({
+  onSwitchChange,
+  name,
+  ...rest
+}: CoreSwitchProps) => {
   return (
     <Box {...rest}>
-      <Switch onChange={handleChange} />
+      <Controller
+        name={name ?? ''}
+        defaultValue={false}
+        render={({ field: { onChange } }) => (
+          <Switch
+            onChange={(
+              _event: React.ChangeEvent<HTMLInputElement>,
+              checked: boolean,
+            ) => {
+              if (onSwitchChange) {
+                onSwitchChange(checked);
+              }
+              onChange(checked);
+            }}
+          />
+        )}
+      />
     </Box>
   );
 };
