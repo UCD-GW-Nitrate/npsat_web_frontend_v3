2349f50e9e22586a1bad697401f7a82c8f6db469
Caden <cnewt2003>
2024-03-19 23:28:57 -0700

    feat(step5): loading indicator and ui complete

--- src/pages/model/steps/Step5/index.tsx
+++ src/pages/model/steps/Step5/index.tsx
@@ -1,6 +1,12 @@
+import { Box, CircularProgress } from '@mui/material';
+import { Result } from 'antd';
 import React, { useEffect } from 'react';
 
+import { CoreButton } from '@/components/core/CoreButton/CoreButton';
+import { CoreText } from '@/components/core/CoreText/CoreText';
 import { useGetModelStatusQuery } from '@/store';
+
+// import styles from './index.css';
 
 interface Step5Props {
   ids: any;
@@ -18,11 +24,31 @@ const Step5 = ({ ids }: Step5Props) => {
     console.log('model data is', data);
   }, [data]);
 
+  const extra = (
+    <>
+      <CoreButton label="View scenario run" variant="contained" />
+      <CoreButton label="Compare with BAU" variant="outlined" />
+      <CoreButton label="Create another scenario" variant="outlined" />
+    </>
+  );
+
   return (
-    <div>
-      <p>loaded</p>
-      {data && data.results[0] && data.results[0].status === 3 && <p>done</p>}
-    </div>
+    <Box sx={{ mx: 'auto' }}>
+      {!(data && data.results[0] && data.results[0].status === 3) && (
+        <Box
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
+          sx={{ mt: 5 }}
+        >
+          <CircularProgress size="4rem" />
+          <CoreText sx={{ mt: 2 }}>Creating Scenario...</CoreText>
+        </Box>
+      )}
+      {data && data.results[0] && data.results[0].status === 3 && (
+        <Result status="success" title="Scenario created" extra={extra} />
+      )}
+    </Box>
   );
 };
 
