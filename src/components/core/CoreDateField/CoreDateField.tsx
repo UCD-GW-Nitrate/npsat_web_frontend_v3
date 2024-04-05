56d7f82fa249d10693d444c896141a92ed2e9831
Caden <cnewt2003>
2024-04-05 10:38:19 -0700

    feat(form): minor changes

--- src/components/core/CoreDateField/CoreDateField.tsx
+++ src/components/core/CoreDateField/CoreDateField.tsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 import type { DateView } from '@mui/x-date-pickers';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import type { Dayjs } from 'dayjs';
 import dayjs from 'dayjs';
 import React, { useState } from 'react';
 import { Controller } from 'react-hook-form';
@@ -10,15 +11,17 @@ import { Controller } from 'react-hook-form';
 export interface CoreDateFieldProps extends BoxProps {
   views?: DateView[];
   name?: string;
+  defaultDate?: Dayjs;
 }
 
 export const CoreDateField = ({
   sx,
   views,
   name,
+  defaultDate,
   ...rest
 }: CoreDateFieldProps) => {
-  const [value, setValue] = useState<dayjs.Dayjs | null>(null);
+  const [value, setValue] = useState<Dayjs | undefined>(defaultDate);
 
   return (
     <Box sx={{ width: 200, ...sx }} {...rest}>
@@ -28,6 +31,8 @@ export const CoreDateField = ({
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePicker
               views={views}
+              minDate={dayjs()}
+              maxDate={dayjs(new Date(2200, 1, 1))}
               onChange={(event) => {
                 onChange(event?.toDate());
                 setValue(event ?? dayjs());
